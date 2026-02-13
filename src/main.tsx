628289e344e1262d2d3f96b7a505c1e8e08a543b
thaikrosa <thaisiks@gmail.com>
2026-02-13 10:34:20 -0300

    perf: remove unused deps, optimize bundle

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,10 @@
 import * as React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
+import "@fontsource/inter/400.css";
+import "@fontsource/inter/500.css";
+import "@fontsource/inter/600.css";
+import "@fontsource/inter/700.css";
 import "./index.css";
 
 const container = document.getElementById("root");
